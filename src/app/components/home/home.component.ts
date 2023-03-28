4e6f4c50e45fe9d39dbd1ba6682905283dfc1233
tqfaleh <rescco14@gmail.com>
2023-03-28 11:28:03 +0300

    ..

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,37 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  public lottieConfig: Object;
-  private anim: any;
-  private animationSpeed: number = 1;
 
-  constructor() {
-      this.lottieConfig = {
-          path: 'assets/pinjump.json',
-          renderer: 'canvas',
-          autoplay: true,
-          loop: true
-      };
-  }
-
-  handleAnimation(anim: any) {
-      this.anim = anim;
-  }
-
-  stop() {
-      this.anim.stop();
-  }
-
-  play() {
-      this.anim.play();
-  }
-
-  pause() {
-      this.anim.pause();
-  }
-
-  setSpeed(speed: number) {
-      this.animationSpeed = speed;
-      this.anim.setSpeed(speed);
-  }
 }
